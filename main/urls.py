5bf7a82a579bbadbd8f2a67e09f807d6c7f6e2e5
Harsh <prajapatiharsh.210@gmail.com>
2024-01-04 01:04:25 +0530

    added redirect for home url

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(pattern_name='welcome', permanent=False)),
     path('welcome/', TemplateView.as_view(template_name='welcome.html'), name='welcome'),
     path('accounts/', include('accounts.urls')),
 ]
